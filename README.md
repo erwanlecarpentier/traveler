c187b1fbc7012638fceb64297eac703c0ddaee21
erwanlecarpentier <erwan.lc2@gmail.com>
2018-07-14 14:47:04 +0200

    Repo re-organized

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Routing environment.
 
 # Dependencies
 
-Non standard libraries used:
+Non-standard libraries used:
 - 'libconfig.h++' available at https://github.com/hyperrealm/libconfig
 - 'Eigen/Dense' available at http://eigen.tuxfamily.org/
 
--- demo/main.cpp
+++ demo/main.cpp
@@ -76,9 +76,9 @@ int main(int argc, char ** argv) {
         std::clock_t c_start = std::clock();
         srand(time(NULL));
         if(argc > 1) {
-            single_run(argv[1]);
+            //single_run(argv[1]);
             //generate_maps(argv[1],1);
-            //sequential_policies(argv[1],run);
+            sequential_policies(argv[1],run);
             //parallel_policies(argv[1],run);
         } else {
             throw no_parameters_path_exception();
